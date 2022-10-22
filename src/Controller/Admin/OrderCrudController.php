6d5c81bfc44d7949ae8a2910bb0ccb9a4ddb451d
jeremy <jeremymacadre@gmail.com>
2022-10-22 19:12:29 +0200

    Modifications mineures, déplacement d'une fonctions dans la class et non dans le controller

---
+++ src/Controller/Admin/OrderCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Order;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class OrderCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Order::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id'),
+            DateField::new('createdAt', 'Passée le')->setFormat('dd-MM-yyyy hh:mm:ss')
+        ];
+    }
+
+}

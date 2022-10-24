dda8c8031d38bf450376e771c5867e92081a2ddc
jeremy <jeremymacadre@gmail.com>
2022-10-24 14:02:00 +0200

    Remise à niveau de la sauvegarde

--- migrations/Version20221023181721.php
+++ migrations/Version20221023181721.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221020142809 extends AbstractMigration
+final class Version20221023181721 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20221020142809 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, is_paid TINYINT(1) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE `order` ADD reference VARCHAR(255) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE `order`');
+        $this->addSql('ALTER TABLE `order` DROP reference');
     }
 }

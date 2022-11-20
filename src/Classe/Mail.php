7eb74667e0ed5a5ae3c58b9deee3ad18897f92ab
Jeremy Macadre <jeremymacadre@gmail.com>
2022-11-20 23:01:28 +0100

    Ajout de la class Mail(), ajout de l'API MailJet afin d'envoyer des mails à mes clients

---
+++ src/Classe/Mail.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Classe;
+
+use Mailjet\Client;
+use Mailjet\Resources;
+
+class Mail
+{
+    private $api_key = '6263dc1ec4d6d7eb07300009a5dabe59';
+    private $api_key_secret = '0092d009e7533076de26c8d67420a76a';
+
+    public function send($to_email, $to_name, $subject, $content)
+    {
+        $mj = new Client($this->api_key, $this->api_key_secret,true,['version' => 'v3.1']);
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => "jeremymacadre@gmail.com",
+                        'Name' => "La Boutique Française"
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $to_email,
+                            'Name' => $to_name
+                        ]
+                    ],
+                    'TemplateID' => 4366447,
+                    'TemplateLanguage' => true,
+                    'Subject' => $subject,
+                    'Variables' => [
+                        'content' => $content,
+                    ]
+                ]
+            ]
+        ];
+        $response = $mj->post(Resources::$Email, ['body' => $body]);
+        $response->success();
+    }
+}
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
